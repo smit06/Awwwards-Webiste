90310a7ea3174d791e5079388357dc16c9599f99
smit06 <smitshah116@gmail.com>
2024-12-29 14:59:59 +0530

    setup of vite project and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+        <h1 className="text-3xl font-bold text-orange-500">
+          Welcome to Awwards Website
+        </h1>
+    </main>
+  )
+}
+  
+export default App;
